3717791694d9c33aeb26f3a12d75fa71a6167fe7
volkanGoksu <volkan.goksu@outlook.com>
2021-05-11 19:25:29 +0300

    db connect

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import nodemon from 'nodemon';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const app = express();
+
+app.listen(process.env.PORT,()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useFindAndModify:true,
+    }).then(()=>console.log('Connect DB'))
+    .catch((err)=>console.log(err));
+
+})
