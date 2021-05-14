bb6a618d3df705bd330904f74bc3132e9780ee36
volkanGoksu <volkan.goksu@outlook.com>
2021-05-14 19:46:13 +0300

    test

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://game-liist.herokuapp.com/',
+  baseURL: 'https://vglistgame.herokuapp.com/',
   withCredentials: true,
 })
 
